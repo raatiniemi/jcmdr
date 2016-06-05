ed6f4249e12e3d36572a812ef5e9c2f9e5c0d7bd
Tobias Raatiniemi <raatiniemi@gmail.com>
2016-06-05 06:45:24 +0200

    document public interface of Argument

--- src/main/java/me/raatiniemi/jcmdr/scheme/annotation/Argument.java
+++ src/main/java/me/raatiniemi/jcmdr/scheme/annotation/Argument.java
@@ -21,10 +21,29 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Annotate method to be included in scheme argument parsing.
+ */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Argument {
+    /**
+     * Define the short name for the argument scheme.
+     *
+     * The short name must be either empty or a single character, if a
+     * more descriptive name is needed, use {@link #longName}.
+     *
+     * @return Short name for the argument scheme.
+     */
     String shortName() default "";
 
+    /**
+     * Define the long name for the argument scheme.
+     *
+     * The long name must be either empty or more than a one character,
+     * if a single character is needed, use {@link #shortName}.
+     *
+     * @return Long name for the argument scheme.
+     */
     String longName() default "";
 }
